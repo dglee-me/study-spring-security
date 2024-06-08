d2de721f65985abf1b3bdd9582fdedf82c6ab0bc
dglee-me <dglee.dev@gmail.com>
2024-06-08 15:13:32 +0900

    init

---
+++ src/main/java/kr/co/dglee/study/StudySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dglee.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringSecurityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudySpringSecurityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kr/co/dglee/study/StudySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dglee.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
